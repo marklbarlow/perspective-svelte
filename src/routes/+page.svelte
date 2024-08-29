575dd3194a9ada6ff20810b68205b9884a8937f3
Mark Barlow <23555996+marklbarlow@users.noreply.github.com>
2024-08-29 20:13:07 +0100

    Initial version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import perspective from '@finos/perspective';
+	import '@finos/perspective-viewer';
+	import '@finos/perspective-viewer-datagrid';
+	import '@finos/perspective-viewer/dist/css/pro.css';
+	import arrow from 'superstore-arrow/superstore.lz4.arrow';
+
+	async function initialise() {
+		const worker = perspective.worker();
+		const viewer = document.querySelector('perspective-viewer');
+		const resp = await fetch(arrow);
+		const buffer = await resp.arrayBuffer();
+		const table = worker.table(buffer);
+		viewer?.load(table);
+	}
+
+	$effect(() => {
+		initialise();
+	});
+</script>
+
+<perspective-viewer style="height:100vh;"></perspective-viewer>
